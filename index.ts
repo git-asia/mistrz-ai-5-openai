89a259299a57e93a20e011bf5b5646f71d9d286e
joanna <tuszynska.joanna@gmail.com>
2024-02-02 20:53:48 +0100

    Adjusted chat instructions

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import {OpenAiChat} from "./open-ai-chat";
 
 (async () => {
-    const chat = new OpenAiChat('Jesteś świetnym klasyfikatorem tekstów. Zwracasz odpowiedzi w formacie json.');
+    const chat = new OpenAiChat('Jesteś świetnym klasyfikatorem tekstów. Zwracasz odpowiedzi w formacie json wywołując funkcje.');
 
-    console.log(await chat.say('Zwróć sentyment tego tekstu jako `sentiment`:\n\n"Nigdy nie nauczę się języka hiszpańskiego."'));
+    console.log(await chat.say('Zwróć sentyment tego tekstu:\n\n"Nigdy nie nauczę się języka hiszpańskiego."'));
     console.log(await chat.say('Zwróć temat tego tekstu jako `topic`'));
     console.log(await chat.say('Zwróć słowo kluczowe tego tekstu jako `keyword`'));
 
